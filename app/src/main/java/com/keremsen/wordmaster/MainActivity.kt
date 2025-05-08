5dd2fec986d7aa6b42976efc14e1a4eb46d23820
keremsenn <senkerem949@gmail.com>
2025-05-09 00:19:48 +0300

    mainactivity düzeltme

--- app/src/main/java/com/keremsen/wordmaster/MainActivity.kt
+++ app/src/main/java/com/keremsen/wordmaster/MainActivity.kt
@@ -3,16 +3,8 @@ package com.keremsen.wordmaster
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.keremsen.wordmaster.navigation.AppNavigation
-import com.keremsen.wordmaster.ui.theme.WordMasterTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
