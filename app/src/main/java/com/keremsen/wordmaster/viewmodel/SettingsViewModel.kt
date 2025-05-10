1e6d023b65afbb5616c7b80b8450ef4d5be41d72
keremsenn <senkerem949@gmail.com>
2025-05-10 11:46:39 +0300

    ses ayarı için view model eklendi

---
+++ app/src/main/java/com/keremsen/wordmaster/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,20 @@
+package com.keremsen.wordmaster.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+
+class SettingsViewModel : ViewModel() {
+    var isSoundOn by mutableStateOf(true)
+        private set
+
+    fun toggleSound() {
+        isSoundOn = !isSoundOn
+    }
+
+    fun setSound(on: Boolean) {
+        isSoundOn = on
+    }
+}
