3385a08bc3b8c300356d61882c474fa7ac818f8c
keremsenn <senkerem949@gmail.com>
2025-05-15 16:17:45 +0300

    splash screen yazıldı

--- app/src/main/java/com/keremsen/wordmaster/view/SplashScreen.kt
+++ app/src/main/java/com/keremsen/wordmaster/view/SplashScreen.kt
@@ -1,2 +1,51 @@
 package com.keremsen.wordmaster.view
 
+
+import androidx.compose.foundation.background
+import com.keremsen.wordmaster.R
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loadinganimation))
+    val progress by animateLottieCompositionAsState(composition = composition,iterations = LottieConstants.IterateForever )
+    LaunchedEffect(Unit) {
+        delay(3000)
+        navController.navigate("MainScreen") {
+            popUpTo("SplashScreen") { inclusive = true }
+        }
+    }
+    Column(modifier = Modifier.fillMaxSize()
+        .background(color = colorResource(id=R.color.LoadingBackGroundColor))) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            LottieAnimation(
+                composition = composition,
+                progress = { progress },
+                modifier = Modifier.size(200.dp)
+            )
+        }
+    }
+
+
+
+}
