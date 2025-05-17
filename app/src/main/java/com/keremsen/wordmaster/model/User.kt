8516f68d67c03ddd7f1bb02c17a2eef9f1f719b0
keremsenn <senkerem949@gmail.com>
2025-05-17 15:32:55 +0300

    User model null verilerde hata yonetımı ıcın guncelleme

--- app/src/main/java/com/keremsen/wordmaster/model/User.kt
+++ app/src/main/java/com/keremsen/wordmaster/model/User.kt
@@ -4,7 +4,7 @@ data class User(
    val uid: String = "",
    val email: String = "",
    val name: String = "",
-   val level: Int = 1,
+   val level: Int = -1,
    val createdAt: String = "",
    val isAdmin: Boolean = false
 )
