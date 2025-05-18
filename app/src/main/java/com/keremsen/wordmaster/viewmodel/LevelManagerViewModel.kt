4189be8d387671b0650a69a0ee60f032a888017a
keremsenn <senkerem949@gmail.com>
2025-05-18 18:26:52 +0300

    levelmanagerview model eklendi

---
+++ app/src/main/java/com/keremsen/wordmaster/viewmodel/LevelManagerViewModel.kt
@@ -0,0 +1,14 @@
+package com.keremsen.wordmaster.viewmodel
+
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+
+class LevelManagerViewModel(private val sharedPreferences: SharedPreferences): ViewModel() {
+    fun saveLevel(level: Int) {
+        sharedPreferences.edit().putInt("user_level", level).apply()
+    }
+
+    fun getLevel(): Int {
+        return sharedPreferences.getInt("user_level", 1)
+    }
+}
