7f0ff91938e86cbed77c93ecd0d687557b9a4cab
keremsenn <senkerem949@gmail.com>
2025-05-27 11:49:58 +0300

    mainacativty guncelleme

--- app/src/main/java/com/keremsen/wordmaster/MainActivity.kt
+++ app/src/main/java/com/keremsen/wordmaster/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.keremsen.wordmaster.navigation.AppNavigation
 import com.keremsen.wordmaster.viewmodel.MusicPlayerViewModel
 import com.keremsen.wordmaster.viewmodel.SettingsViewModel
@@ -15,13 +18,22 @@ class MainActivity : ComponentActivity() {
     private val musicPlayerViewModel: MusicPlayerViewModel by viewModels()
     private val wordViewModel: WordViewModel by viewModels()
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // AdMob'u başlat
+        MobileAds.initialize(this)
+        val configuration = RequestConfiguration.Builder()
+            .setTestDeviceIds(listOf(AdRequest.DEVICE_ID_EMULATOR)) // Emülatör için test cihaz ID'si
+            .build()
+        MobileAds.setRequestConfiguration(configuration)
+
         lifecycle.addObserver(musicPlayerViewModel)
+
         setContent {
-            AppNavigation(settingsViewModel ,musicPlayerViewModel,wordViewModel)
+                    AppNavigation(settingsViewModel, musicPlayerViewModel, wordViewModel)
         }
     }
 }
+
 
